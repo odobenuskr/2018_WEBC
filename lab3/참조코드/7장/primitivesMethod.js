ef026f178893121b7999e002a5848add6e9a7f78
sungjae <odobenuseKR@gmail.com>
2018-06-03 17:23:48 +0900

    delete not important thing

--- lab3/참조코드/7장/primitivesMethod.js
+++
@@ -1,5 +0,0 @@
-let stringA = '과자|1500원';
-console.log(stringA.split('|'));
-
-let stringB = new String('과자|1500원');
-console.log(stringB.split('|'));
--- lab3/참조코드/7장/stringMistake.js
+++
@@ -1,5 +0,0 @@
-// 변수를 선언합니다.
-let string = 'abcdefg';
-// 출력합니다.
-string.toUpperCase();
-console.log(string);
